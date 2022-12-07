bf296b13114765f395f6ce46e962e526ea5795cf
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-12-08 01:38:01 +0800

    Update for FSB 0.6.1

--- src/main/java/me/flashyreese/mods/fabricskyboxes_interop/mixin/SkyboxManagerAccessor.java
+++ src/main/java/me/flashyreese/mods/fabricskyboxes_interop/mixin/SkyboxManagerAccessor.java
@@ -2,13 +2,14 @@ package me.flashyreese.mods.fabricskyboxes_interop.mixin;
 
 import io.github.amerebagatelle.fabricskyboxes.SkyboxManager;
 import io.github.amerebagatelle.fabricskyboxes.skyboxes.AbstractSkybox;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-import java.util.ArrayList;
+import java.util.Map;
 
 @Mixin(value = SkyboxManager.class, remap = false)
 public interface SkyboxManagerAccessor {
-    @Accessor("skyboxes")
-    ArrayList<AbstractSkybox> getSkyboxes();
+    @Accessor("skyboxMap")
+    Map<Identifier, AbstractSkybox> getSkyboxes();
 }
